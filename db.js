b985d7b258f1ebd5fac309016754cb4aae9ed2ff
EdyBe <edy.belingher@spark.school>
2025-03-19 21:08:38 +1300

    Update db.js

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js'; // Import Supabase client
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
