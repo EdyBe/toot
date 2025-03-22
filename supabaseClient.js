e14ba61b41a2cc610bcd4fa279fd570fa7f74333
EdyBe <edy.belingher@spark.school>
2025-03-22 15:53:42 +1300

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,10 +1,14 @@
 import { createClient } from '@supabase/supabase-js';
+import dotenv from 'dotenv';
+
+// Load environment variables
+dotenv.config();
 
 // Initialize Supabase client with environment variables
-const SUPABASE_URL = process.env.SUPABASE_URL; // Accessing environment variables directly
-const SUPABASE_KEY = process.env.SUPABASE_KEY; // Accessing environment variables directly
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_KEY = process.env.SUPABASE_KEY;
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
 // Export the supabase client for use in other files
-export {supabase};
+export { supabase };
